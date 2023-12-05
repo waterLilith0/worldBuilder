28990090d47dd79dddb1d04f6cc591cefd3f33ab
waterLilith0 <mililith0@pm.me>
2023-12-05 08:39:46 +0100

    did some changes to category to automatically be assigned random unique ids

--- src/main/java/com/worldbuilder/Category.java
+++ src/main/java/com/worldbuilder/Category.java
@@ -1,8 +1,11 @@
 package com.worldbuilder;
 
+import java.security.SecureRandom;
+
 public class Category {
     protected String name;
-    protected int catID; // this is a random number; each category has it's own individual id in the object
+    protected SecureRandom random = new SecureRandom();
+    protected int catID = random.nextInt(); // this is a random number; each category has it's own individual id in the object
 
     Category(int catID, String name) {
         this.catID = catID;
@@ -10,4 +13,8 @@ public class Category {
     }
 
     Category() {}
+
+    public int getCatID() {
+        return catID;
+    }
 }
