8bd1f823d2e4c20b68dc1e330a28aa9fd3262ddf
waterLilith <mililith0@pm.me>
2024-04-09 11:00:26 +0200

    funny

--- src/main/java/com/worldbuilder/MainExec.java
+++ src/main/java/com/worldbuilder/MainExec.java
@@ -9,6 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+// TODO GAYYYYYYY
+
 public class MainExec extends Application {
     @Override
     public void start(Stage stage) throws Exception {
